51ce94128b1966e079613bb2d0af07d32dfacedd
Glenn Adams <glenn@skynav.com>
2021-12-04 22:10:17 -0600

    fix for cargo.io publish

---
+++ crates/mmpm/src/lib.rs
@@ -0,0 +1,8 @@
+//! # mms-mmpm
+//!
+//! The `mms-mmpm` crate provides an implementation of the Manufacturing Message Specification (MMS)
+//! Protocol Machine (MMPM) as defined by ISO 9506-1:2003 and ISO 9506-2:2003.
+//!
+//! This version is a placeholder for work in progress.
+//!
+
